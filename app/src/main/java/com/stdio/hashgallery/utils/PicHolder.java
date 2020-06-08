35f3896c0dea6abda866cfcaf297624c84fe7cbf
Std1o <37378410+Std1o@users.noreply.github.com>
2020-06-08 23:05:11 +0300

    gallery

---
+++ app/src/main/java/com/stdio/hashgallery/utils/PicHolder.java
@@ -0,0 +1,26 @@
+package com.stdio.hashgallery.utils;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.stdio.hashgallery.R;
+
+
+/**
+ *Author CodeBoy722
+ *
+ * picture_Adapter's ViewHolder
+ */
+
+public class PicHolder extends RecyclerView.ViewHolder{
+
+    public ImageView picture;
+
+    PicHolder(@NonNull View itemView) {
+        super(itemView);
+
+        picture = itemView.findViewById(R.id.image);
+    }
+}
