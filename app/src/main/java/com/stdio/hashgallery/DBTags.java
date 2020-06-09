c38a40c1d8c87b209779b87cf20a73d21e458bd0
Std1o <37378410+Std1o@users.noreply.github.com>
2020-06-09 18:36:29 +0300

    Create DBTags.java

---
+++ app/src/main/java/com/stdio/hashgallery/DBTags.java
@@ -0,0 +1,36 @@
+package com.stdio.hashgallery;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBTags extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "tagsDB";
+    public static final String TABLE_TAGS = "tags";
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_URI = "uri";
+    public static final String KEY_TAGS = "tags";
+
+    public DBTags(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_TAGS + "(" + KEY_ID
+                + " integer primary key,"  + KEY_URI + " text," +  KEY_TAGS + " text" + ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_TAGS);
+
+        onCreate(db);
+
+    }
+}
